e43812a8f86a02065a66b975b12159160e65cd48
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-12-30 20:19:00 +0100

    Added layout to BlockPosition form

---
+++ src/Repositories/LayoutRepository.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Mustache\Repositories;
+
+use VitesseCms\Database\Models\FindValue;
+use VitesseCms\Database\Models\FindValueIterator;
+use VitesseCms\Mustache\Models\Layout;
+use VitesseCms\Mustache\Models\LayoutIterator;
+
+class LayoutRepository
+{
+    public function findAll(
+        ?FindValueIterator $findValues = null,
+        bool $hideUnpublished = true
+    ): LayoutIterator
+    {
+        Layout::setFindPublished($hideUnpublished);
+        Layout::addFindOrder('name');
+        $this->parsefindValues($findValues);
+
+        return new LayoutIterator(Layout::findAll());
+    }
+
+    public function findByDatagroup(
+        string $datagroupId,
+        ?FindValueIterator $findValues = null,
+        bool $hideUnpublished = true
+    ): LayoutIterator
+    {
+        if ($findValues === null) :
+            $findValues = new FindValueIterator([]);
+        endif;
+        $findValues->append(new FindValue('datagroup',$datagroupId));
+
+        return $this->findAll($findValues, $hideUnpublished);
+    }
+
+    protected function parsefindValues(?FindValueIterator $findValues = null): void
+    {
+        if ($findValues !== null) :
+            while ($findValues->valid()) :
+                $findValue = $findValues->current();
+                Layout::setFindValue(
+                    $findValue->getKey(),
+                    $findValue->getValue(),
+                    $findValue->getType()
+                );
+                $findValues->next();
+            endwhile;
+        endif;
+    }
+}
