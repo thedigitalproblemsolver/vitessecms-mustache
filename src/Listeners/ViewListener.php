83d31b59e4f7299a88914ca888cb48fcfec44077
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2022-11-25 16:33:51 +0100

    Phalcon 3 > 5 changes

--- src/Listeners/ViewListener.php
+++ src/Listeners/ViewListener.php
@@ -7,6 +7,7 @@ use Phalcon\Mvc\View;
 use VitesseCms\Content\Models\Item;
 use VitesseCms\Core\Services\ViewService;
 use VitesseCms\Mustache\DTO\RenderLayoutDTO;
+use VitesseCms\Mustache\DTO\RenderPartialDTO;
 use VitesseCms\Mustache\DTO\RenderTemplateDTO;
 use VitesseCms\Mustache\Repositories\LayoutRepository;
 use VitesseCms\Mustache\Services\RenderService;
@@ -74,17 +75,17 @@ class ViewListener
         $this->modules = $modules;
     }
 
-    public function renderPartial(Event $event, string $partial, Item $item): string
+    public function renderPartial(Event $event, RenderPartialDTO $renderPartialDTO, Item $item): string
     {
-        if (is_file($this->templateDir . 'views/partials/fields/' . $partial . '.mustache')):
+        if (is_file($this->templateDir . 'views/partials/fields/' . $renderPartialDTO->getPartial() . '.mustache')):
             $renderTemplateDTO = new RenderTemplateDTO(
-                $partial,
+                $renderPartialDTO->getPartial(),
                 $this->templateDir . 'views/partials/fields/',
                 ['currentItem', $item]
             );
         else :
             $renderTemplateDTO = new RenderTemplateDTO(
-                $partial,
+                $renderPartialDTO->getPartial(),
                 str_replace($this->baseDir, '', $this->coreTemplateDir) . 'views/partials/fields/',
                 ['currentItem' => $item]
             );
