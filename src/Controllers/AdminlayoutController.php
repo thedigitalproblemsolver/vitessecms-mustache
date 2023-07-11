994f27143f63899504da0c8dc40e92ff48954cbb
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-07-11 09:31:58 +0200

    Removed old code

--- src/Controllers/AdminlayoutController.php
+++ src/Controllers/AdminlayoutController.php
@@ -117,23 +117,4 @@ class AdminlayoutController extends AbstractControllerAdmin implements
     {
         return 'mustache/src/Resources/admin/views/';
     }
-
-    /*public function onConstruct()
-    {
-        parent::onConstruct();
-
-        $this->class = Layout::class;
-        $this->classForm = LayoutForm::class;
-    }
-
-    public function editAction(
-        string       $itemId = null,
-        string       $template = 'editForm',
-        string       $formTemplatePath = 'mustache/src/Resources/admin/views/',
-        AbstractForm $form = null
-    ): void
-    {
-        $this->assets->setEventLoader(ViewEnum::ASSETS_LOAD_GRID_EDITOR);
-        parent::editAction($itemId, $template, $formTemplatePath, $form);
-    }*/
 }
